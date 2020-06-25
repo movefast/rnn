0e12b297e8f1aba0790a5bb4a02aacf95dbe9a72
Derek Li <xzli@cedar1.cedar.computecanada.ca>
2020-06-25 13:36:28 -0700

    Untracked files issue resolved to fix .gitignore

---
+++ run_experiment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --account=def-whitem
+
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --mem=4G
+#SBATCH --time=12:00:00
+
+#SBATCH --mail-user=xli@ualberta.ca
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+#SBATCH --job-name=fpp_experiment
+
+
+
+python run_long_dep_job.py --agent_idxes="[4]"
