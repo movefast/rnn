188bf2f07fb84972181d51469818596f5ff2d19a
Derek Li <xzli@cedar5.cedar.computecanada.ca>
2020-07-03 02:39:57 -0700

    updated run script

--- run_experiment_v1.sh
+++ run_experiment_v1.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-#SBATCH --account=def-whitem
-
-#SBATCH --gres=gpu:1
-#SBATCH --nodes=1
-#SBATCH --mem=4G
-#SBATCH --time=12:00:00
-
-#SBATCH --mail-user=xli@ualberta.ca
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-
-#SBATCH --job-name=fpp_experiment
-
 T_list=(5 10 20)
 lr_list=(3e-3 1e-3 3e-4 1e-4)
 for T in ${T_list[@]}; do
     for lr in ${lr_list[@]}; do
-        sbatch -p serial_requeue -t 10 --mem=200 --wrap="python run_single_job.py --agent_idxes='[2]' --T=$T --lr=$lr"
+        sbatch --account=def-whitem --gres=gpu:1 --nodes=1 --mem=4G --time=24:00:00 --mail-user=xli@ualberta.ca --mail-type=END --mail-type=FAIL --job-name=TBPTT_experiment_${T}_${lr} --wrap="python run_single_job.py --agent_idxes='[2]' --T=$T --lr=$lr"
         sleep 1
     done
 done
